192f9d048a8bab8f5eccfce182b96ac8faaaeaf0
leandro <le.fernandezc@duocuc.cl>
2025-05-08 12:02:21 -0400

    agregamos el msvc producto

---
+++ msvc-producto/src/test/java/com/edutech/msvc/producto/MsvcProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
