b973e8bbe69ba793effff3bd319ee3a8caf8f33f
lefernandezc <le.fernandezc@duocuc.cl>
2025-05-22 12:18:24 -0400

    actualizacion

---
+++ msvc-inscripcion/src/main/java/com/msvc_inscripcion/Exceptions/InscripcionException.java
@@ -0,0 +1,7 @@
+package com.msvc_inscripcion.Exceptions;
+
+public class InscripcionException extends RuntimeException {
+    public InscripcionException(String message) {
+        super(message);
+    }
+}
