1cfc74a91f293c020d7ad847dd5131b3a06ccd8d
lefernandezc <le.fernandezc@duocuc.cl>
2025-05-23 16:42:15 -0400

    services completados

--- msvc-inscripcion/src/main/java/com/msvc_inscripcion/Repositories/InscripcionRepository.java
+++ msvc-inscripcion/src/main/java/com/msvc_inscripcion/Repositories/InscripcionRepository.java
@@ -4,6 +4,10 @@ import com.msvc_inscripcion.Models.Entities.Inscripcion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InscripcionRepository extends JpaRepository<Inscripcion,Long> {
+
+    List<Inscripcion> findByIdAlumno(Long idAlumno);
 }
--- msvc-inscripcion/src/main/java/com/msvc_inscripcion/Services/InscripcionServiceImpl.java
+++ msvc-inscripcion/src/main/java/com/msvc_inscripcion/Services/InscripcionServiceImpl.java
@@ -1,13 +1,11 @@
 package com.msvc_inscripcion.Services;
 
 import com.msvc_inscripcion.Clients.AlumnoClientRest;
-import com.msvc_inscripcion.Clients.NotasClientRest;
-import com.msvc_inscripcion.Clients.ProfesorClientRest;
+import com.msvc_inscripcion.Dtos.AlumnoDTO;
 import com.msvc_inscripcion.Dtos.InscripcionDTO;
 import com.msvc_inscripcion.Exceptions.InscripcionException;
 import com.msvc_inscripcion.Models.Alumno;
-import com.msvc_inscripcion.Models.Notas;
-import com.msvc_inscripcion.Models.Profesor;
+import com.msvc_inscripcion.Models.Entities.Inscripcion;
 import com.msvc_inscripcion.Repositories.InscripcionRepository;
 import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +22,6 @@ public class InscripcionServiceImpl implements InscripcionService {
     @Autowired
     private AlumnoClientRest alumnoClientRest;
 
-    @Autowired
-    private NotasClientRest notasClientRest;
-
-    @Autowired
-    private ProfesorClientRest profesorClientRest;
-
     @Override
     public List<InscripcionDTO> findAll(){
         return this.inscripcionRepository.findAll().stream().map(inscripcion ->{
@@ -41,17 +33,37 @@ public class InscripcionServiceImpl implements InscripcionService {
                 throw new InscripcionException("Alumnno buscado no exite");
             }
 
-            Notas notas = null;
-            Profesor profesor = null;
-            try{
-                notas = this.notasClientRest.findById(alumno.getIdNotas());
+            AlumnoDTO alumnoDTO = new AlumnoDTO();
+            alumnoDTO.setCorreo(alumno.getCorreo());
+            alumnoDTO.setRun(alumno.getRun());
+            alumnoDTO.setNombre(alumno.getNombre());
 
-            }
+            InscripcionDTO inscripcionDTO = new InscripcionDTO();
+            inscripcionDTO.setAlumno(alumnoDTO);
+            return inscripcionDTO;
 
-        }
+        }).toList();
     }
 
+    @Override
+    public Inscripcion findById(Long id){
+        return this.inscripcionRepository.findById(id).orElseThrow(
+                () -> new InscripcionException("La inscripcion co id: " + id + " no se encuentra en la base de datos")
+        );
+    }
 
+    @Override
+    public Inscripcion save(Inscripcion inscripcion){
+        try{
+            Alumno alumno = this.alumnoClientRest.findById(inscripcion.getIdAlumno());
+        }catch (FeignException ex) {
+            throw new InscripcionException("Existe problemas con la asoción inscripcion alumno");
+        }
+        return this.inscripcionRepository.save(inscripcion);
+    }
 
-
+    @Override
+    public List<Inscripcion> findByAlumnoId (Long alumnoId){
+        return this.inscripcionRepository.findByIdAlumno(alumnoId);
+    }
 }
