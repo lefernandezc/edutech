8ad99852e28dcbc0f0f964f701661bac7a8dce6c
lefernandezc <le.fernandezc@duocuc.cl>
2025-06-09 11:01:54 -0400

    avance de service test

--- msvc-alumno/src/test/java/com/msvc_alumno/service/AlumnoServiceTest.java
+++ msvc-alumno/src/test/java/com/msvc_alumno/service/AlumnoServiceTest.java
@@ -1,10 +1,13 @@
 package com.msvc_alumno.service;
 
+import com.msvc_alumno.dtos.AlumnoDTO;
 import com.msvc_alumno.model.entites.Alumno;
 import com.msvc_alumno.repositories.AlumnoRepository;
 import com.msvc_alumno.services.AlumnoServiceImpl;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,6 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AlumnoServiceTest {
@@ -30,5 +37,29 @@ public class AlumnoServiceTest {
     @BeforeEach
     public void setUp(){
         Faker faker = new Faker(Locale.of("es","CL"));
+        for(int i=0;i<100;i++){
+            Alumno alumno = new Alumno();
+            alumno.setIdAlumno((long) i+1);
+            alumno.setNombre(faker.name().fullName());
+            alumno.setCorreo(faker.internet().emailAddress());
+
+            String numeroString = faker.idNumber().valid().replaceAll("-","");
+            String ultimo = numeroString.substring(numeroString.length()-1);
+            String restante = numeroString.substring(0,numeroString.length()-1);
+
+            alumno.setRun(restante+"-"+ultimo);
+            this.alumnoList.add(alumno);
+        }
+        this.alumnoPrueba = this.alumnoList.get(0);
+    }
+
+    @Test
+    @DisplayName("Devuele todos los alumnos")
+    public void shouldFindAllAlumnos(){
+        when(alumnoRepository.findAll()).thenReturn(this.alumnoList);
+        List<AlumnoDTO> result = alumnoService.findAll();
+        assertThat(result).hasSize(200);
+        assertThat(result).contains(this.alumnoPrueba);
+
     }
 }
