8cc4b0c4880ca8aeb7d8180666710b3c37074fb1
edutech <caceressimon70@gmail.com>
2025-06-23 17:35:06 -0400

    actualizado

--- msvc-alumno/src/main/java/com/msvc_alumno/controllers/AlumnoControllerV2.java
+++ msvc-alumno/src/main/java/com/msvc_alumno/controllers/AlumnoControllerV2.java
@@ -1,6 +1,7 @@
 package com.msvc_alumno.controllers;
 
 import com.msvc_alumno.assemblers.AlumnoModelAssembler;
+import com.msvc_alumno.dtos.AlumnoDTO;
 import com.msvc_alumno.model.entites.Alumno;
 import com.msvc_alumno.services.AlumnoService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,8 +33,14 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Tag(name = "alumno V2", description = "Operaciones CRUD de inscripcion hateoas")
 public class AlumnoControllerV2 {
 
+    @Autowired
+    private AlumnoService alumnoService;
+
+    @Autowired
+    private AlumnoModelAssembler alumnoModelAssembler;
+
     @GetMapping
-    @Operation(summary = "Obtiene todos las alumno", description = "Devuelve un List de Inscripciones en el Body")
+    @Operation(summary = "Obttiene todo de alumno", description = "Devuelve un listado de alumno")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
@@ -45,89 +52,66 @@ public class AlumnoControllerV2 {
             )
     })
     public ResponseEntity<CollectionModel<EntityModel<Alumno>>> findAll(){
-        List<EntityModel<Alumno>> entityModels = this.al
+        List<EntityModel<Alumno>> entityModels = this.alumnoService.findAll()
                 .stream()
-                .map(inscripcionModelAssembler::toModel)
+                .map(alumnoModelAssembler::toModel)
                 .toList();
-
-        CollectionModel<EntityModel<Inscripcion>> collectionModel = CollectionModel.of(
+        CollectionModel<EntityModel<Alumno>> collectionModel = CollectionModel.of(
                 entityModels,
-                linkTo(methodOn(InscripcionControllerV2.class).findAll()).withSelfRel()
+                linkTo(methodOn(AlumnoControllerV2.class).findAll()).withSelfRel()
         );
-
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(collectionModel);
     }
-
-    @GetMapping("/{id}")
-    @Operation(summary = "Obtiene una inscripcion", description = "A través del id suministrado devuelve la inscripcion con esa id")
-    @ApiResponses( value = {
+    @GetMapping("/id")
+    @Operation(summary = "Obtiene un alumno", description = "A traves del id suministrado devuelve la inscripcion con esa id")
+    @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
                     description = "Operacion existosa",
                     content = @Content(
                             mediaType = MediaTypes.HAL_JSON_VALUE,
-                            schema = @Schema(implementation = Inscripcion.class)
+                            schema = @Schema(implementation = Alumno.class)
                     )
             ),
             @ApiResponse(
                     responseCode = "404",
-                    description = "Inscripcion no encontrado, con el id suministrado",
+                    description = "Alumno no se encuentra, con el id suministrado",
                     content = @Content(
                             mediaType = "application/json",
-                            schema =  @Schema(implementation = ErrorDTO.class)
+                            schema = @Schema(implementation = Error.class)
                     )
             )
     })
     @Parameters(value = {
-            @Parameter(name="id", description = "Este es el id unico de la inscripcion", required = true)
+            @Parameter(name = "id", description = "Este es el id unico del alumno", required = true)
     })
-    public ResponseEntity<EntityModel<Inscripcion>> findById(@PathVariable Long id){
-        EntityModel<Inscripcion> entityModel = this.inscripcionModelAssembler.toModel(
-                this.inscripcionService.findById(id)
+    public ResponseEntity<EntityModel<Alumno>> findById(@PathVariable Long id){
+        EntityModel<Alumno> entityModel = this.alumnoModelAssembler.toModel(
+                this.alumnoService.findById(id)
         );
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(entityModel);
     }
-
     @PostMapping
     @Operation(
-            summary = "Guarda una inscripcion",
-            description = "Con este método podemos enviar los datos mediante un body y realizar el guardado"
+            summary = "Guarda un alumno",
+            description = "con este metodo podemos enviar los datos mediante un body y realizar el guardado"
     )
-    @ApiResponses( value = {
+    @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "201",
                     description = "Guardado exitoso",
                     content = @Content(
                             mediaType = MediaTypes.HAL_JSON_VALUE,
-                            schema = @Schema(implementation = Inscripcion.class)
+                            schema = @Schema(implementation = Alumno.class)
                     )
             ),
             @ApiResponse(
                     responseCode = "409",
-                    description = "La inscripcion guardada ya se encuentra en la base de datos",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = ErrorDTO.class)
-                    )
+                    description = "El alumno guardado"
             )
     })
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            description = "inscripcion a crear",
-            content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = Inscripcion.class)
-            )
-    )
-    public ResponseEntity<EntityModel<Inscripcion>> save(@Valid @RequestBody Inscripcion inscripcion){
-        Inscripcion inscripNew = this.inscripcionService.save(inscripcion);
-        EntityModel<Inscripcion> entityModel = this.inscripcionModelAssembler.toModel(inscripNew);
-
-        return ResponseEntity
-                .created(linkTo(methodOn(InscripcionControllerV2.class).findById(inscripNew.getIdInscripcion())).toUri())
-                .body(entityModel);
-    }
 }
--- msvc-profesor/src/main/java/com/msvc_profesor/controllers/ProfesorControllerV2.java
+++ msvc-profesor/src/main/java/com/msvc_profesor/controllers/ProfesorControllerV2.java
@@ -1,15 +1,141 @@
 package com.msvc_profesor.controllers;
 
+import com.msvc_profesor.assemblers.ProfesorModelAssembler;
+import com.msvc_profesor.models.entilies.Profesor;
+import com.msvc_profesor.services.ProfesorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/v2/profesor")
 @Validated
 @Tag(
         name = "Medico API HATEOAS",
-        description = "Aquí se generar todos los métodos CRUD para medico"
+        description = "Aquí se generar todos los métodos CRUD para profesor"
 )
 public class ProfesorControllerV2 {
+
+    @Autowired
+    private ProfesorService profesorService;
+
+    @Autowired
+    private ProfesorModelAssembler profesorModelAssembler;
+
+    @GetMapping
+    @Operation(summary = "Obtiene todo de profesor", description = "Devielve un lista de profesor")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operacion existosa",
+                    content = @Content(
+                            mediaType = MediaTypes.ALPS_JSON_VALUE,
+                            schema = @Schema(implementation = Profesor.class)
+                    )
+
+
+            )
+    })
+
+    public ResponseEntity<CollectionModel<EntityModel<Profesor>>> findAll(){
+        List<EntityModel<Profesor>> entityModels = this.profesorService.findAll()
+                .stream()
+                .map(profesorModelAssembler::toModel)
+                .toList();
+
+        CollectionModel<EntityModel<Profesor>> collectionModel =CollectionModel.of(
+                entityModels,
+                linkTo(methodOn(ProfesorControllerV2.class).findAll()).withSelfRel()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(collectionModel);
+
+    }
+
+    @GetMapping("{/id}")
+    @Operation(summary = "Obtiene un profesor", description = "A traves del id suministrado devuelve el profesor con esa id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operacion existosa",
+                    content = @Content(
+                            mediaType = MediaTypes.HAL_JSON_VALUE,
+                            schema = @Schema(implementation = Profesor.class)
+                    )
+
+
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Profesor no se  encuentra, con la id suministrada",
+                    content = @Content(
+                            mediaType = "applicacion/json",
+                            schema = @Schema(implementation = Error.class)
+                    )
+            )
+    })
+    @Parameters(value = {
+            @Parameter(name = "id", description = "este es el id unico de profesor", required = true)
+    })
+    public ResponseEntity<EntityModel<Profesor>> findById(@PathVariable Long id){
+        EntityModel<Profesor> entityModel = this.profesorModelAssembler.toModel(
+                this.profesorService.findById(id)
+        );
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(entityModel);
+    }
+    @PostMapping
+    @Operation(
+            summary = "Guarda un profesor",
+            description = "con este metodo podemos enviar los datos mediante un body y realizar el guardado"
+    )
+    @ApiResponses(value ={
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "guardado existo",
+                    content = @Content(
+                            mediaType = MediaTypes.HAL_JSON_VALUE,
+                            schema = @Schema(implementation = Profesor.class)
+                    )
+
+            )
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "profesor a crear",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Profesor.class)
+            )
+    )
+    public ResponseEntity<EntityModel<Profesor>> save(@Valid @RequestBody Profesor profesor){
+        Profesor profeNew = this.profesorService.save(profesor);
+        EntityModel<Profesor> entityModel =this.profesorModelAssembler.toModel(profeNew);
+
+        return ResponseEntity
+                .created(linkTo(methodOn(ProfesorControllerV2.class).findById(profeNew.getIdProfesor())).toUri())
+                .body(entityModel);
+    }
+
 }
