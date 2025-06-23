f511e3072ec83f3b42aaa04c9ce41fd956be747c
edutech <caceresimon70@gmail.com>
2025-06-23 12:31:40 -0400

    actualizar

--- msvc-alumno/src/test/java/com/msvc_alumno/controllers/AlumnoControllerTest.java
+++ msvc-alumno/src/test/java/com/msvc_alumno/controllers/AlumnoControllerTest.java
@@ -2,6 +2,7 @@ package com.msvc_alumno.controllers;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.msvc_alumno.model.entites.Alumno;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +29,38 @@ public class AlumnoControllerTest {
         int alumnoCount = documentContext.read("$.length()");
         assertThat(alumnoCount).isEqualTo(1000);
     }
+
+
+    @Test
+    public void shouldReturnAnAlumnoWhenFindById(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/alumno/1", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number idALumno = documentContext.read("$.idAlumno");
+        assertThat(idALumno).isEqualTo(1);
+
+
+    }
+
+    @Test
+    public void shouldReturnAnAlumnoWithUnknownId(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/alumno/9999", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number status = documentContext.read("$.status");
+        assertThat(status).isEqualTo(404);
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldCreateANewAlumno(){
+        Alumno alumno = new Alumno();
+        ResponseEntity<String> response = restTemplate.postForEntity("/api/v1/alumno",alumno, String.class);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number idAlumno = documentContext.read("$.idAlumno");
+        assertThat(idAlumno).isEqualTo(3);
+    }
+
 
 }
--- msvc-profesor/src/test/java/com/msvc_profesor/controllers/ProfesorControllerTest.java
+++ msvc-profesor/src/test/java/com/msvc_profesor/controllers/ProfesorControllerTest.java
@@ -3,6 +3,8 @@ package com.msvc_profesor.controllers;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.msvc_profesor.models.entilies.Profesor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -22,13 +25,44 @@ public class ProfesorControllerTest {
 
     @Test
     @DisplayName("Debe entregar un listado con todos los medicos")
-    public void shouldReturnAllMedicoWhenListIsRequired(){
+    public void shouldReturnAllProfesorWhenListIsRequired(){
         ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/profesor",String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         DocumentContext documentContext = JsonPath.parse(response.getBody());
         int profesorCount = documentContext.read("$.length()");
         assertThat(profesorCount).isEqualTo(1000);
+    }
+
+
+    @Test
+    public void shouldReturnAnProfesorWhenFindById(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/profesor/1", String.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number idProfesor = documentContext.read("$.idProfesor");
+        Assertions.assertThat(idProfesor).isEqualTo(1);
+
+    }
+
+    @Test
+    public void shouldReturnAnprofesorWithUnknownId(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/api/v1/profesor/9999", String.class);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number status = documentContext.read("$.status");
+        Assertions.assertThat(status).isEqualTo(404);
+    }
+
+    @Test
+    @DirtiesContext
+    public void shouldCreateANewProfesor(){
+        Profesor profesor = new Profesor();
+        ResponseEntity<String> response = restTemplate.postForEntity("/api/v1/profesor",profesor, String.class);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number idProfesor = documentContext.read("$.idProfesor");
+        Assertions.assertThat(idProfesor).isEqualTo(3);
     }
 
 }
